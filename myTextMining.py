88c3b514bbff87ba0ba8d777f1e4da4c69e0e6e9
hyejeong1019 <126781268+hyejeong1019@users.noreply.github.com>
2024-10-23 11:59:40 +0900

    Add files via upload

---
+++ myTextMining.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import streamlit as st
+from konlpy.tag import Komoran
+from collections import Counter
+
+def load_corpus_from_csv(filename, column):
+    data_df = pd.read_csv(filename)
+    corpus = list(data_df[column])
+    return corpus
+
+def tokenize_data(corpus):
+    komo = Komoran()
+    my_tags = ['NNP', 'NNG', 'VA']
+    my_stopwords = ['없', '같', '많', '영화', "!!"]
+
+    result_tokens = []
+    for text in corpus:
+        tokens = [word for word, tag in komo.pos(text) if tag in my_tags and word not in my_stopwords]
+        result_tokens.extend(tokens)
+    return result_tokens
+
+@st.cache_data
+def analyze_word_freq(corpus):
+    result_tokens = tokenize_data(corpus)
+    counter = Counter(result_tokens)
+    return counter
+
+def generate_wordcloud(counter, num_words):
+        # 워드클라우드 시각화
+    from wordcloud import WordCloud
+    font_path = "c:/Windows/fonts/malgun.ttf"
+
+    # WordCloud 객체 생성
+    wordcloud = WordCloud(
+        font_path = font_path,
+        #max_font_size = 200,
+        width = 800, #이미지 너비 지정
+        height = 600, #이미지 높이 지정
+        max_words=num_words,
+        background_color='ivory' #이미지 배경색 지정
+    )
+    wordcloud=wordcloud.generate_from_frequencies(counter)
+    return wordcloud
---
+++ wordFreqVisualizer.py
@@ -0,0 +1,69 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
+
+from matplotlib import font_manager, rc
+
+import myTextMining as tm
+
+
+
+@st.cache_data
+
+def visualize_barhgraph(counter, num_words):
+    font_path = "c:/Windows/Fonts/malgun.ttf"
+    font_name = font_manager.FontProperties(fname=font_path).get_name()
+    rc('font', family=font_name)
+
+    word_list = [word for word, _ in counter.most_common(num_words)]
+    count_list = [count for _, count in counter.most_common(num_words)]
+
+    fig, ax = plt.subplots()
+    ax.barh(word_list[::-1], count_list[::-1])
+    st.pyplot(fig)
+
+def visualize_wordcloud(counter, num_words):
+
+    wordcloud = tm.generate_wordcloud(counter, num_words)
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud)
+    ax.axis('off')
+    st.pyplot(fig)
+
+#######################################
+# 웹 대시보드
+#######################################
+
+st.sidebar.write("## 설정")
+with st.sidebar.form('my_form'):
+    data_file = st.file_uploader("파일 선택", type=['csv'])
+    column_name = st.text_input('데이터가 있는 컬럼명', value='review')
+    freq = st.checkbox('빈도수 그래프')
+    num_freq_words = st.slider('단어 수', 10, 50, 20, 1)
+    wc = st.checkbox('워드클라우드')
+    num_wc_words = st.slider('단어 수', 20, 500, 50, 10)
+    submitted = st.form_submit_button('분석 시작')
+
+st.title('단어 빈도수 시각화')
+status = st.info('분석할 파일을 업로드하고, 시각화 수단을 선택한 후 "분석 시작" 버튼을 클릭하세요.')
+
+# 메인 화면에 결과 출력
+
+if submitted:
+    if data_file:
+        status.info('데이터를 분석 중입니다.')
+        #data_file = "./data/daum_movie_review.csv"
+        corpus = tm.load_corpus_from_csv(data_file, column_name)  
+        counter = tm.analyze_word_freq(corpus)
+        status.info('분석이 완료되었습니다.')
+
+        if freq: visualize_barhgraph(counter, num_freq_words)
+        if wc: visualize_wordcloud(counter, num_wc_words)
+        if not freq and not wc:
+            st.warning('빈도수 그래프 또는 워드클라우드 중 하나 이상을 선택하세요.')
+            # df = pd.DataFrame(counter.most_common(20), columns=['단어', '빈도수'])
+            # df
+    else:
+        st.warning('분석할 데이터 파일을 업로드 한 후 분석 시작하세요.')
+    
