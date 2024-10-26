b58d9af62cc76b1907a794764531277a005a918b
hyejeong1019 <126781268+hyejeong1019@users.noreply.github.com>
2024-10-26 11:25:47 +0900

    Add files via upload

---
+++ mySTVisualizer.py
@@ -0,0 +1,52 @@
+from matplotlib import font_manager, rc
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import streamlit as st
+import myTextMining as tm
+
+def is_running_on_streamlit_cloud():
+
+    isCloud=False
+    
+    #return "STREAMLIT_SERVER_PORT" in os.environ
+    
+    # server_address = os.environ.get("SERVER_NAME", "localhost")
+    # if server_address in ["localhost", "127.0.0.1"]:
+    #      return False
+    # return True
+
+    return isCloud
+
+@st.cache_data
+def visualize_barhgraph(counter, num_words):
+
+    if is_running_on_streamlit_cloud():
+        font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+        font_name = 'NanumGothic'
+        font_manager.fontManager.addfont(font_path)
+    else:
+        font_path = "c:/Windows/Fonts/malgun.ttf"
+        font_name = font_manager.FontProperties(fname=font_path).get_name() 
+
+    rc('font', family=font_name)
+
+    word_list = [word for word, _ in counter.most_common(num_words)]
+    count_list = [count for _, count in counter.most_common(num_words)]
+
+    fig, ax = plt.subplots()
+    ax.barh(word_list[::-1], count_list[::-1])
+    st.pyplot(fig)
+
+def visualize_wordcloud(counter, num_words):
+        
+    if is_running_on_streamlit_cloud():
+        font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'      
+    else:    
+        font_path = "c:/Windows/fonts/malgun.ttf"
+
+    wordcloud = tm.generate_wordcloud(counter, num_words, font_path)
+
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud)
+    ax.axis('off')
+    st.pyplot(fig)
--- wordFreqVisualizer.py
+++ wordFreqVisualizer.py
@@ -1,57 +1,10 @@
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+
 import streamlit as st
 import pandas as pd
 import os
 
-from matplotlib import font_manager, rc
 import myTextMining as tm
-
-isCloud = True
-
-def is_running_on_streamlit_cloud():
-    #return "STREAMLIT_SERVER_PORT" in os.environ
-    
-    # server_address = os.environ.get("SERVER_NAME", "localhost")
-    # if server_address in ["localhost", "127.0.0.1"]:
-    #      return False
-    # return True
-
-    return isCloud
-
-@st.cache_data
-def visualize_barhgraph(counter, num_words):
-
-    if is_running_on_streamlit_cloud():
-        font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
-        font_name = 'NanumGothic'
-        font_manager.fontManager.addfont(font_path)
-    else:
-        font_path = "c:/Windows/Fonts/malgun.ttf"
-        font_name = font_manager.FontProperties(fname=font_path).get_name() 
-
-    rc('font', family=font_name)
-
-    word_list = [word for word, _ in counter.most_common(num_words)]
-    count_list = [count for _, count in counter.most_common(num_words)]
-
-    fig, ax = plt.subplots()
-    ax.barh(word_list[::-1], count_list[::-1])
-    st.pyplot(fig)
-
-def visualize_wordcloud(counter, num_words):
-        
-    if is_running_on_streamlit_cloud():
-        font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'      
-    else:    
-        font_path = "c:/Windows/fonts/malgun.ttf"
-
-    wordcloud = tm.generate_wordcloud(counter, num_words, font_path)
-
-    fig, ax = plt.subplots()
-    ax.imshow(wordcloud)
-    ax.axis('off')
-    st.pyplot(fig)
+import mySTVisualizer as sv
 
 #######################################
 # 웹 대시보드
@@ -87,8 +40,8 @@ if submitted:
         counter = tm.analyze_word_freq(corpus)
         status.info('분석이 완료되었습니다.')
 
-        if freq: visualize_barhgraph(counter, num_freq_words)
-        if wc: visualize_wordcloud(counter, num_wc_words)
+        if freq: sv.visualize_barhgraph(counter, num_freq_words)
+        if wc: sv.visualize_wordcloud(counter, num_wc_words)
         if not freq and not wc:
             st.warning('빈도수 그래프 또는 워드클라우드 중 하나 이상을 선택하세요.')
             # df = pd.DataFrame(counter.most_common(20), columns=['단어', '빈도수'])
